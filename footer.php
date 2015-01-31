b88d1da23d5e65da6eddb3fd5cb1979868b3d503
Kcchouette <kcchouette@gmail.com>
2015-01-31 16:08:45 +0000

    Just add the url in the footer

--- footer.php
+++ footer.php
@@ -1,6 +1,5 @@
 <?php
 echo '
 <div class="CopyRight">XDCC Fetcher. Copyright &copy; 2015 Kcchouette</div>
-<div class="CopyRight">Comments &amp; downloads &amp; Bugs  @ https://github.com/Kcchouette/XDCC-fetcher/</div>';
+<div class="CopyRight">Comments &amp; downloads &amp; Bugs  @  <a href="https://github.com/Kcchouette/XDCC-fetcher/" target="_blank">https://github.com/Kcchouette/XDCC-fetcher/</a></div>';
 ?>
-
