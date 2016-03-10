c734cac94bc7e8cd3b1c11ed245d86f65bccb043
Kcchouette <Kcchouette@users.noreply.github.com>
2016-03-10 12:31:44 +0100

    Change footer style

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <?php
-echo '
-<div class="CopyRight">XDCC Fetcher. Copyright &copy; 2015 Kcchouette</div>
-<div class="CopyRight">Comments &amp; downloads &amp; Bugs  @  <a href="https://github.com/Kcchouette/XDCC-fetcher/" target="_blank">https://github.com/Kcchouette/XDCC-fetcher/</a></div>';
+  echo '
+  <footer>
+    <p class="omgcenter">XDCC Fetcher. Copyright &copy; 2015 Kcchouette<br>Comments &amp; downloads &amp; Bugs on <a href="https://github.com/Kcchouette/XDCC-fetcher/" target="_blank">Github</a></p>
+  </footer>';
 ?>
